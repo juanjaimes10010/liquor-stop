b0cdffa9869d94a0741b47a5d0e2247f31a4999c
juan <juanjaimes10010@gmail.com>
2025-05-14 01:31:49 -0500

    test 2

--- app/admin/test/page.tsx
+++ app/admin/test/page.tsx
@@ -47,7 +47,7 @@ export default function Test({ children }: { children: React.ReactNode }) {
                         </svg>
                     </button>
                 </div>      
-                
+
                 <div className="category__content">
                     {category.subcategories.map( (subctegory, i) => {
                         return <article className="subcategory" key={i}>
@@ -125,7 +125,7 @@ export default function Test({ children }: { children: React.ReactNode }) {
             {dialog == 'addCategoryDialog' && (
                 <form className="dialog" onSubmit={submit}>
                     <button className="dialog__close" onClick={closeDialog}>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
                             <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
                         </svg>
                     </button>
